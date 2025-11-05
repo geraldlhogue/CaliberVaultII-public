77103711a6666411d9afe7a4a35350fde835febb
Gerry Hogue <gerry@sageforce.net>
2025-11-04 14:21:28 -1000

    chore(test): remove stray duplicate directories and stage prior changes

--- firearms-inventory-tracking-1 (4)/src/contexts/AppContext.tsx
+++
@@ -1,4 +0,0 @@
-// THIS FILE IS DEPRECATED - USE AppContextNew.tsx INSTEAD
-// This file re-exports from AppContextNew for backward compatibility
-
-export { useAppContext, AppProvider } from './AppContextNew';
